d2b6ab4ff3cb68482d4af4ea362cbe06e5ba9795
Ricardo Seiti <ricardo.seiti@microchip.com>
2025-05-27 18:30:10 -0300

    Adicionado funcao exibir_saudacao

--- saudacoes.py
+++ saudacoes.py
@@ -1,12 +1,29 @@
+import datetime
+
 def exibir_saudacao():
     print("--- Máquina de Saudações Colaborativa ---")
     print("Olá! Seja bem-vindo(a)!")
     print("---------------------------------------")
 
+def saudacao_periodo():
+    print("++++++++++++++")
+    datetime.datetime.now().hour #pega o valor da hora atual
+    a = 33
+    b = 200
+    c = 400
+    if a >= b >= c:
+        print("BOM DIA!!!")
+    elif a == b:
+        print("BOA TARDE!!!")
+    else:
+        print("BOA NOITE!!!")
+
+
 def main():
     exibir_saudacao()
     # Adicione suas funções de saudação aqui!
     # Exemplo:
+    saudacao_periodo()
     # saudacao_manha()
     # frase_inspiradora()
 
